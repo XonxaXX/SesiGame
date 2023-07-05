e4fa70a0e8c94bba095a9d272f4440c1f45a691d
bArpini <131875301+XonxaXX@users.noreply.github.com>
2023-07-05 11:06:29 -0300

    botão start feito

---
+++ SesiGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ativa painel do menu
+        menuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //funçao do botão start 
+    public void StartGame()
+    {
+        //carrega a cena do game
+        SceneManager.LoadScene(nomeCena);
+    }
+}
